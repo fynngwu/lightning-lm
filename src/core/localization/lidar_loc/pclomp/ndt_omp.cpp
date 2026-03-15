8e1173d20e89e722291a58b7de086590f558d0f5
wufy <you@example.com>
2026-03-15 20:33:24 +0800

    设置一个flag可以让loc部分可以不用编译，livox暂时只使用slam部分

--- src/core/localization/lidar_loc/pclomp/ndt_omp.cpp
+++ src/core/localization/lidar_loc/pclomp/ndt_omp.cpp
@@ -1,5 +1,8 @@
 #include "ndt_omp.h"
 #include "ndt_omp_impl.hpp"
 
+#include "common/point_def.h"
+
 template class pclomp::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>;
 template class pclomp::NormalDistributionsTransform<pcl::PointXYZI, pcl::PointXYZI>;
+template class pclomp::NormalDistributionsTransform<PointXYZIT, PointXYZIT>;
--- src/core/localization/lidar_loc/pclomp/voxel_grid_covariance_omp.cpp
+++ src/core/localization/lidar_loc/pclomp/voxel_grid_covariance_omp.cpp
@@ -1,5 +1,8 @@
 #include "voxel_grid_covariance_omp.h"
 #include "voxel_grid_covariance_omp_impl.hpp"
 
+#include "common/point_def.h"
+
 template class pclomp::VoxelGridCovariance<pcl::PointXYZ>;
 template class pclomp::VoxelGridCovariance<pcl::PointXYZI>;
+template class pclomp::VoxelGridCovariance<PointXYZIT>;
